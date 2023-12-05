b86f9e5394f9af3525cc8bf6bd721cd56304e201
yeonoel <yeopevroguinoel@gmail.com>
2023-12-05 15:44:47 +0000

    dependency injection

--- hopital/src/main/java/ma/enset/hopital/entities/Patient.java
+++ hopital/src/main/java/ma/enset/hopital/entities/Patient.java
@@ -8,6 +8,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,8 +23,12 @@ public class Patient {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotEmpty
+	@Size(min = 4, max = 20)
 	private String nom;
+	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
 	private boolean malade;
+	@Min(100)
 	private int score;
 }
