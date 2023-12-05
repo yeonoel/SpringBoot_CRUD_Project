afb1018077f422e303a375dfca2eb6ff52e372a2
yeonoel <yeopevroguinoel@gmail.com>
2023-12-05 15:43:04 +0000

    add methods: savePatient, editePatient, addPatient

--- hopital/src/main/java/ma/enset/hopital/web/PatientControler.java
+++ hopital/src/main/java/ma/enset/hopital/web/PatientControler.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.enset.hopital.entities.Patient;
 import ma.enset.hopital.repository.PatientRepository;
@@ -34,5 +37,28 @@ public class PatientControler {
 	public String delete(Long id, String keyword, int page) {
 		patientRepository.deleteById(id);
 		return "redirect:/index?page="+page+"&keyword="+keyword;
+	}
+	@GetMapping("/")
+	public String home() { return "redirect:/index";}
+
+	@GetMapping("/formulairepatient")
+	public String formulairepatient(Model model) {
+		model.addAttribute("patient", new Patient());
+		return "formulairepatient";
+	}
+	@PostMapping("/savePatient")
+	public String savePatient(@Valid Patient patient, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "formulairepatient";
+		}
+		patientRepository.save(patient);
+		return "redirect:/index?keyword="+patient.getNom();
+	}
+	
+	@GetMapping("/editePatient")
+	public String editePatient(Model model, @RequestParam(name = "id") Long id) {
+		Patient patient = patientRepository.findById(id).get();
+		model.addAttribute("patient", patient);
+		return "editePatient";
 	}
 }
